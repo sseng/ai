cc15e29bcb39b96f50f04eb03bf1bb1bc007e2b8
sseng <sseng81@gmail.com>
2013-02-05 18:38:24 -0800

    string game

---
+++ StringGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, state> letters = new Dictionary<string, state>();
+        }
+    }
+}
