a05e49f3fb8e6937e57ea77de15dbfd3d0c7c83d
Sothearith <sseng81@gmail.com>
2014-10-02 12:30:25 -0700

    added markov
    sentence randomizer

---
+++ markov/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Markov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            markov test = new markov();
+            //test.add("one", "two", "three");
+            //test.add("one", "two", "four");
+            //test.add("one", "two", "five");
+
+            //string a = test.lookup("one", "two");
+
+            //Console.Write(a);
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            //string source = "a cat is a dog is a cow is a boy is cat";
+            StreamReader sr = new StreamReader("prideandprejudice.txt");            
+            String line = sr.ReadToEnd();
+            //Console.WriteLine(line);
+            string source = line;
+            
+
+
+            test.storeWords(source);
+
+            string b = test.lookup("a", "cat");
+            string c = test.lookup("cat", b);
+            string d = test.lookup("is", "a");
+
+            
+            string first = test.sentence[0];
+            string second = test.sentence[1];
+
+            Console.Write(first + " " + second + " ");
+
+            for (int i = 0; i < 100; i++)
+            {
+                string third = test.lookup(first, second);
+
+                if (third == "")
+                {
+                    
+                    int position = rand.Next(test.sentence.Count());
+                    third = test.sentence[position];
+                    
+                    if (position < test.sentence.Length - 1)
+                    {
+                        string fourth = test.sentence[position + 1];
+                        Console.Write(third + " ");
+                        Console.Write(fourth + " ");
+                        first = third;
+                        second = fourth;
+                        i++;
+                    }
+                }
+                else
+                {
+                    Console.Write(third + " ");
+                    first = second;
+                    second = third;
+                }
+            }
+            Console.ReadKey();
+        }        
+    }
+}
---
+++ markov/markov.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Markov
+{
+    public class markov
+    {
+        public string[] sentence;
+        Dictionary<string, Dictionary<string, HashSet<string>>> words;
+        Random rand = new Random();
+
+        public markov()
+        {
+             words = new Dictionary<string, Dictionary<string, HashSet<string>>>();
+             //sentence = new ArrayList();
+        }
+
+        public void add(string word1, string word2, string word3)
+            {   
+                if (!words.ContainsKey(word1)){
+                    words.Add(word1, new Dictionary<string,HashSet<string>>());
+                }
+                if (!words[word1].ContainsKey(word2)){
+                    words[word1].Add(word2, new HashSet<string>());
+                }
+                words[word1][word2].Add(word3);
+            }
+
+        public string lookup(string w1, string w2)
+        { 
+            if (words.ContainsKey(w1)){
+                if (words[w1].ContainsKey(w2)){
+                    HashSet<string> w3 = words[w1][w2];
+                    return w3.ElementAt(rand.Next(w3.Count));
+                }
+            }
+            //return words.Keys.ElementAt(rand.Next(words.Count));
+            return "";
+            //throw new System.Exception();
+        }
+        
+        public void storeWords(string source)
+        {
+            //string[] seperator = new string[] { " " };
+            sentence = source.Split("\r\n ".ToCharArray() , StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < sentence.Length - 2; i++)
+            {
+                this.add(sentence[i], sentence[i + 1], sentence[i + 2]);                
+            }            
+        }
+    }
+}
