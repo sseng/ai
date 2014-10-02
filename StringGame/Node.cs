13d6d2839a506e303bb5930d84b19d5a6f7401cc
Sothearith <sseng81@gmail.com>
2014-10-02 11:34:10 -0700

    update
    long forgotten update.

---
+++ StringGame/Node.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringGame 
+{
+    class Node : IComparable<Node>, IEquatable<Node>
+    {
+        private Node parent;
+        private int f;
+        private int g;
+        private int h;
+        private string gameString;
+
+        public Node(string s) { gameString = s; }
+
+        public string GameString
+        {
+            set { gameString = value; }
+            get { return gameString; }
+        }
+        
+        public Node Parent
+        {
+            set { parent = value; }
+            get { return parent; }
+        }
+        public int F
+        {
+            set { f = value; }
+            get { return f; }
+        }
+        public int G
+        {
+            set { g = value; }
+            get { return g; }
+        }
+        public int H
+        {
+            set { h = value; }
+            get { return h; }            
+        }       
+
+        public void SetH(Node goal)
+        {
+            char[] goalChar = goal.GameString.ToString().ToCharArray();
+            char[] currentChar = GameString.ToString().ToCharArray();
+            int currentH = 0;
+
+            for (int i = 0; i < goalChar.Length; i++)
+            {
+                if (goalChar[i] != currentChar[i])
+                    currentH++;
+            }
+            this.h = (currentH);
+        }
+       
+        public int CompareTo(Node other)
+        {
+            if (this.f < other.f) 
+                return -1;
+            else if (this.f > other.f) 
+                return 1;
+            else 
+                return 0;
+        }
+
+        public override string ToString()
+        {
+            return gameString;
+        }
+
+        public bool Equals(Node obj)
+        {
+            if (this.gameString == obj.gameString)
+                return true;
+
+            return false;
+        }
+
+    }
+}
+
--- StringGame/Program.cs
+++ StringGame/Program.cs
@@ -9,7 +9,96 @@ namespace StringGame
     {
         static void Main(string[] args)
         {
-            //Dictionary<string, state> letters = new Dictionary<string, state>();
+            string input;
+            string result;
+            List<Node> closed = new List<Node>();
+            PriorityQueue<Node> open = new PriorityQueue<Node>();            
+
+            Console.WriteLine("enter initial string set: ");
+            input = Console.ReadLine();
+            Console.WriteLine("Enter desired string set: ");
+            result = Console.ReadLine();
+
+            Node start = new Node(input);
+            Node goal = new Node(result);
+            
+            start.G = 0; //set G to amount of nodes traversed aka priority
+            start.SetH(goal); //set the h value based on the goal node's value
+
+            Node current = start;
+            open.Enqueue(current); //add current node into open list
+
+            while (open.Peek() != goal) 
+            {
+                //generate children based on items in open list (current)
+                List<Node> Children = getChildren(current, closed, open);
+
+                current = open.Dequeue(); //pop current node
+                closed.Add(current); //add current node into closed list
+
+                //Console.WriteLine(current.GameString);
+
+                foreach (Node child in Children)
+                {
+                    //child.SetH(goal);
+                    open.Enqueue(child);
+                } 
+
+                if (current.Equals(goal))
+                {
+                    Console.WriteLine("found path:");
+                    Node last = current;
+                    List<Node> list = new List<Node>();
+                    while (last.Parent != null)
+                    {
+                        list.Add(last);
+                        last = last.Parent;
+                    }
+                    list.Add(last);
+                    list.Reverse();
+                    foreach (Node n in list)
+                        Console.WriteLine(n.ToString());
+                    break;
+                }
+
+                if (open.isEmpty())
+                {
+                    Console.WriteLine("failed to find path");
+                    break;
+                }
+
+                if (Children == null)
+                {
+                    current = open.Dequeue(); //set first element of open list to current and pop
+                    continue;
+                }             
+            }
+            Console.ReadKey(true);
+        }
+
+        static List<Node> getChildren(Node current, List<Node> closedList, PriorityQueue<Node> openList)
+        {           
+            string gameString = current.GameString;
+            List<Node> children = new List<Node>();
+
+            for (int i = 0; i < gameString.Length - 1; i++)
+            {
+                char[] charString = gameString.ToCharArray();                
+                
+                char temp = charString[i];
+                charString[i] = charString[i+1];
+                charString[i+1] = temp;
+
+                string s = new string(charString);
+                Node childNode = new Node(s);
+                childNode.G = current.G + 1;
+                childNode.Parent = current;
+
+                //if openlist and closed list does not contain new node, add to children
+                if (!openList.Contains(childNode) && !closedList.Contains(childNode) && !childNode.Equals(current) )
+                    children.Add(childNode);
+            }
+            return children;
         }
     }
 }
